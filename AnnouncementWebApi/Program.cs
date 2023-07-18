f4240b6e2f0b94ca5f662db9e6714e7581e189d7
Andriy Borkovich <andrii.borkovych.pz.2021@lpnu.ua>
2023-07-18 22:23:26 +0300

    code refactoring

--- AnnouncementWebApi/Program.cs
+++ AnnouncementWebApi/Program.cs
@@ -6,22 +6,22 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 builder.Services.AddDbContext<AnnouncementContext>(opt =>
     opt.UseInMemoryDatabase("Announcements"));
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-   // app.UseSwagger();
-    //app.UseSwaggerUI();
     app.UseDeveloperExceptionPage();
 }
+
 app.UseDefaultFiles();
+
 app.UseStaticFiles();
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
